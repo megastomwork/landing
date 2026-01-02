08b4ea32ce8f51bdab81f46556aeb634b1a44a7a
GineTik <densheva18072003@gmail.com>
2026-01-02 12:02:52 +0200

    Fix all TypeScript errors with optional CONFIG values

--- src/shared/components/ui-kit/refresh-route-on-save.tsx
+++ src/shared/components/ui-kit/refresh-route-on-save.tsx
@@ -1,11 +1,11 @@
-'use client'
-import { CONFIG } from '@/shared/constants/client-config.constants'
-import { RefreshRouteOnSave as PayloadLivePreview } from '@payloadcms/live-preview-react'
-import { useRouter } from 'next/navigation'
-import React from 'react'
+'use client';
+import { CONFIG } from '@/shared/constants/client-config.constants';
+import { RefreshRouteOnSave as PayloadLivePreview } from '@payloadcms/live-preview-react';
+import { useRouter } from 'next/navigation';
+import React from 'react';
 
 export const RefreshRouteOnSave: React.FC = () => {
-  const router = useRouter()
+  const router = useRouter();
 
   console.log(CONFIG.SERVER_URL);
   console.log('w:', window?.location?.origin);
@@ -17,7 +17,7 @@ export const RefreshRouteOnSave: React.FC = () => {
         console.log('w2:', window?.location?.origin);
         router.refresh();
       }}
-      serverURL={CONFIG.SERVER_URL}
+      serverURL={CONFIG.SERVER_URL || ''}
     />
-  )
-}
+  );
+};
